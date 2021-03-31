6665ce373a5af2c1f2f701c5cb4b11c51a644196
Stella Vilar <vilar.stella@gmail.com>
2021-03-31 16:17:50 +0200

    Display Ligue 1 ranking

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
+import env from "react-dotenv";
+import axios from 'axios';
 import '../styles/index.scss';
 
 import Header from './Header';
@@ -7,6 +9,28 @@ import Homepage from './Homepage';
 import Ligue1 from './ligue1/Ligue1Classement';
 
 function App() {
+  //GET LIGUE 1 CLASSEMENT WITH RAPIDAPI DATA:
+  const [ classement, setClassement ] = useState([]);
+  const getFrenchRanking = () => {
+    axios.get('https://heisenbug-ligue-1-live-scores-v1.p.rapidapi.com/api/ligue1/table', {
+      headers: {
+        "x-rapidapi-key": env.RAPID_API_KEY,
+	      "x-rapidapi-host": env.RAPID_API_HOST,
+	      "useQueryString": true
+      }
+    })
+    .then((res) => {
+      if (res.error) throw new Error(res.error);
+      setClassement(res.data.records);
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  };
+
+  useEffect(() => {getFrenchRanking()}, []);
+
+
   return (
     <div className="App">
       <Route exact path="/">
@@ -15,7 +39,7 @@ function App() {
       </Route>
       <Route exact path="/classement/ligue1/">
         <Header />
-        <Ligue1 />
+        <Ligue1 classement={classement} />
       </Route>
     </div>
   );
--- src/components/ligue1/Ligue1Classement.js
+++ src/components/ligue1/Ligue1Classement.js
@@ -1,11 +1,73 @@
 import React from 'react';
-import env from "react-dotenv";
+import { Paper, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-function Ligue1() {
-    console.log(env.RAPID_API_KEY)
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      textAlign: 'center',
+    },
+    firstRow: {
+        textAlign: 'left'
+    }
+  }));
+
+function Ligue1({classement}) {
+    const classes = useStyles();
+    
+    //Map classement array
+    const ranking = classement.map((rank, index) =>
+        <Grid container spacing={0} key={index}>
+            <Grid item xs={2}>
+                <Paper className={classes.firstRow}>{rank.team}</Paper>
+            </Grid>
+            <Grid item xs={1}>
+                <Paper className={classes.paper}>{rank.points}</Paper>
+            </Grid>
+            <Grid item xs={1}>
+                <Paper className={classes.paper}>{rank.played}</Paper>
+            </Grid>
+            <Grid item xs={1}>
+                <Paper className={classes.paper}>{rank.win}</Paper>
+            </Grid>
+            <Grid item xs={1}>
+                <Paper className={classes.paper}>{rank.draw}</Paper>
+            </Grid>
+            <Grid item xs={1}>
+                <Paper className={classes.paper}>{rank.loss}</Paper>
+            </Grid>
+            <Grid item xs={1}>
+                <Paper className={classes.paper}>{rank.goalsFor - rank.goalsAgainst}</Paper>
+            </Grid>
+            
+        </Grid>
+    );
+
     return (
         <div className="league">
-            ligue 1
+            <Grid container spacing={0}>
+                <Grid item xs={2}>
+                    <Paper className={classes.paper}>Club</Paper>
+                </Grid>
+                <Grid item xs={1}>
+                    <Paper className={classes.paper}>Pts</Paper>
+                </Grid>
+                <Grid item xs={1}>
+                    <Paper className={classes.paper}>J</Paper>
+                </Grid>
+                <Grid item xs={1}>
+                    <Paper className={classes.paper}>G</Paper>
+                </Grid>
+                <Grid item xs={1}>
+                    <Paper className={classes.paper}>N</Paper>
+                </Grid>
+                <Grid item xs={1}>
+                    <Paper className={classes.paper}>P</Paper>
+                </Grid>
+                <Grid item xs={1}>
+                    <Paper className={classes.paper}>+/-</Paper>
+                </Grid>
+            </Grid>
+            {ranking}
         </div>
     );
 };
