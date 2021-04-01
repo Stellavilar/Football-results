1d7686e248a16e3938c4012838e8165d23d9525f
Stella Vilar <vilar.stella@gmail.com>
2021-04-01 23:08:06 +0200

    Css settings for images

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,41 +1,9 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
-// import { Typography, Button } from '@material-ui/core';
-// import { Link } from 'react-router-dom';
-
 
 function Homepage() {
 
-    //Get titles clicked on tab menu
-    // const titles = useSelector(state => state );
-
     return (
-        <div className="homepage">
-                {/* <div className="homepage-dashboard">
-                    <Typography variant="h2"> {titles.title.name}</Typography> 
-                    <div className="homepage-items">
-                        {titles.title.name ?  
-                        <>
-                        <Link to={`/classement/${titles.title.name.replace(/\s+/g, '').toLowerCase()}`}>
-                            <Button variant="contained">
-                                {titles.title.item1}
-                            </Button>
-                        </Link>
-                        <Link to={`/calendrier/${titles.title.name.replace(/\s+/g, '').toLowerCase()}`}>
-                            <Button variant="contained">
-                                {titles.title.item2}
-                            </Button>
-                        </Link>
-                        <Link to={`/buteurs/${titles.title.name.replace(/\s+/g, '').toLowerCase()}`}>
-                            <Button variant="contained">
-                                {titles.title.item3}
-                            </Button>
-                        </Link>
-                        </> 
-                        : null}
-                    </div>
-                </div> */}
-        </div>
+        <div className="homepage"></div>
     );
 };
 
