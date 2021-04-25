b5db9c13bb147cf9902e9d612ec880d4e8e2246f
Stella Vilar <vilar.stella@gmail.com>
2021-04-25 23:03:02 +0200

    Update grid for scorers page

--- src/components/ScorersPage.js
+++ src/components/ScorersPage.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Typography } from '@material-ui/core';
+import { Container, Row, Col } from 'react-bootstrap';
 import { v4 as uuid } from 'uuid';
 import axios from 'axios';
 
@@ -16,6 +17,7 @@ function ScorersPage() {
         axios.get(`buteurs/${league}`)
             .then((res) => {
                 setGetScorer(res.data.scorers)
+                console.log(res.data)
             })
             .catch((err)=> {
                 console.log(err)
@@ -24,25 +26,27 @@ function ScorersPage() {
     useEffect(() => {getScorers()}, []);
 
     return (
-        <div className="grid">
-            <Typography variant="h3">Meilleurs buteurs</Typography>
-            <div className="table-head">
-                    <div className="logo"></div>
-                    <div className="club">Nom</div>
-                    <div className="club">Club</div>
-                    <div className="club">Buts</div>
-                    <div className="penalty-head">Penalties</div>
-            </div>
+        <Container fluid>
+            <Typography variant="h5">Meilleurs buteurs</Typography>
+            <Row className="show-grid" style={{ backgroundColor:'#ecf0f1', padding: '10px', fontWeight:'bold', borderRadius: '15px 15px 0px 0px'}} >
+                <Col md={1} xs={1}></Col>
+                <Col md={3} xs={3}>Nom</Col>
+                <Col md={1} xs={1}></Col>
+                <Col md={4} xs={4}>Club</Col>
+                <Col md={2} xs={2}>Buts</Col>
+                <Col md={1} xs={1}>Pen.</Col>
+            </Row>
             {getScorer.map((scorer) => 
-            <div className="table-rows" key={uuid()}>
-                <div className="number-row">{scorer.number}</div>
-                <div className="club-row">{scorer.playerName}</div>
-                <div className="club-row">{scorer.club}</div>
-                <div className="result-row">{scorer.goals}</div>
-                <div className="result-row">{scorer.penalties}</div>
-            </div>
+            <Row style={{ padding: '10px 0px'}} key={uuid()} >
+                <Col md={1} xs={1} style={{ fontWeight: 'bold'}}> {scorer.number} </Col>
+                <Col md={3} xs={3} style={{ fontWeight: 'bold'}}> {scorer.playerName} </Col>
+                <Col md={1} xs={1}><img src ={scorer.clubLogo} alt="logo" style={{ width: '20px'}} /></Col>
+                <Col md={4} xs={4} >  {scorer.club} </Col>
+                <Col md={2} xs={2} > {scorer.goals} </Col>
+                <Col md={1} xs={1} > {scorer.penalties} </Col>
+            </Row>
             )}
-        </div>
+        </Container>
     );
 };
 
